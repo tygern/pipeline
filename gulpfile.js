fc6afebd1f2cbb0d878aae7610d8b50a38ca5000
Tyson Gern <pair+tgern@pivotal.io>
2016-01-20 17:46:41 +0000

    Add initial style and markup tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,26 @@
-var gulp = require('gulp');
+(function () {
+    'use strict';
 
-gulp.task('default', function () {
-    console.log('Hello world');
-});
+    var gulp = require('gulp');
+    var sass = require('gulp-sass');
+
+    gulp.task('default', function () {
+        console.log('Hello world');
+    });
+
+    gulp.task('style', function () {
+        return gulp.src('app/style/app.scss')
+            .pipe(sass())
+            .pipe(gulp.dest('dist/style'));
+    });
+
+    gulp.task('markup', function () {
+        return gulp.src('app/index.html')
+            .pipe(gulp.dest('dist'));
+    });
+
+    gulp.task('watch', ['style', 'markup'], function () {
+        gulp.watch('app/**/*.scss', ['style']);
+        gulp.watch('app/index.html', ['markup']);
+    });
+})();
