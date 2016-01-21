ccc226fa3798897d1f3b0b51ef426b984f69a59d
Tyson Gern <pair+tgern@pivotal.io>
2016-01-21 16:49:43 +0000

    Add typescript support

--- README.md
+++ README.md
@@ -6,11 +6,12 @@ A gulp pipeline for angular applications with sass support. Work in progress.
 
 - Run the following commands to get started
 ```bash
-npm install -g gulp bower
+npm install -g gulp bower typescript
 npm install
 bower install
+tsd install
 ```
-- To compile sass, run `gulp style`.
-- To compile html, run `gulp markup`.
+- To build, run `gulp build`.
+- To watch for changes, run `gulp watch`.
 
-To compile sass and html, while watching for changes, run `gulp watch`. Results are saved in the `dist` directory.
+Results are saved in the `dist` directory.
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 (function () {
     'use strict';
 
-    var gulp = require('gulp');
-    var sass = require('gulp-sass');
+    var gulp = require('gulp'),
+        sass = require('gulp-sass'),
+        tsProject = require('./tsconfig.json'),
+        ts = require('gulp-typescript'),
+        wiredep = require('wiredep').stream,
+        del = require('del');
 
     gulp.task('default', function () {
         console.log('Hello world');
@@ -16,11 +20,25 @@
 
     gulp.task('markup', function () {
         return gulp.src('app/index.html')
+            .pipe(wiredep({}))
             .pipe(gulp.dest('dist'));
     });
 
-    gulp.task('watch', ['style', 'markup'], function () {
+    gulp.task('scripts', function () {
+        return gulp.src(['typings/tsd.d.ts', 'app/**/*.ts'])
+            .pipe(ts(tsProject))
+            .pipe(gulp.dest('dist/scripts'));
+    });
+
+    gulp.task('clean', function () {
+        return del(['dist']);
+    });
+
+    gulp.task('build', ['clean', 'style', 'markup', 'scripts']);
+
+    gulp.task('watch', ['build'], function () {
         gulp.watch('app/**/*.scss', ['style']);
         gulp.watch('app/index.html', ['markup']);
+        gulp.watch('app/**/*.ts', ['scripts']);
     });
 })();
